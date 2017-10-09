0c79095c41a79a99f6dd21a0bcf20bc163901c96
THEJOSHBOX2000\Josh <joshlorenzen49@gmail.com>
2017-10-08 19:50:00 -0700

    Chunk generation now multithreaded

--- Landscape.py
+++ Landscape.py
@@ -1,4 +1,4 @@
-import sys, pygame, Perlin, math
+import sys, pygame, Perlin, math, threading
 pygame.init()
 
 size = width, height = 800, 800
@@ -9,6 +9,12 @@ screen = pygame.display.set_mode(size)
 #CONSTANTS
 CHUNK_SIZE = 128
 PIXEL_SIZE = 4
+EXITING = False
+
+surfaces = {}
+chunkQueue = []
+chunkCond = threading.Condition()
+chunkLock = threading.Lock()
 
 def getColor(height):
 	if height <= 0.20:
@@ -42,6 +48,29 @@ def genChunk(x, y):
 					surface.set_at((i * PIXEL_SIZE + xOffset, j * PIXEL_SIZE + yOffset), color)
 	return surface
 
+#endless loop that handles generating chunks in the chunk queue
+def chunkThread():
+	while not EXITING: #don't get stuck if exiting here either
+		chunkCond.acquire()
+		while chunkQueue == [] and not EXITING: #don't get stuck if exiting
+			chunkCond.wait()
+		if EXITING: break #helps prevent errors when exiting
+		chunkCoords = chunkQueue.pop(0)
+		chunkCond.release()
+		surface = genChunk(chunkCoords[0], chunkCoords[1])
+		chunkLock.acquire()
+		surfaces[chunkCoords[0], chunkCoords[1]] = surface
+		chunkLock.release()
+
+#function to help cleanup chunkThread for exiting
+def cleanup():
+	global EXITING
+	EXITING = True #tell the thread to exit
+	chunkCond.acquire() #acquire this just to notify..
+	chunkCond.notify() #make sure the thread wakes up so it can end
+	chunkCond.release() #just in case
+	chunkThread.join() #make sure it ends before the main thread
+
 screen.fill((0, 0, 0))
 screenX = 0
 screenY = 0
@@ -49,16 +78,20 @@ screenY = 0
 perlin = Perlin.Perlin(350)
 y = 0
 
-surfaces = {}
+chunkThread = threading.Thread(target=chunkThread)
+chunkThread.start()
 
 while True:
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
+		if event.type == pygame.QUIT: 
+			cleanup()
+			sys.exit()
 
 	inKeys = pygame.key.get_pressed()
 
 	if inKeys[pygame.K_ESCAPE]:
-		sys.exit()
+		cleanup()
+		break
 	if inKeys[pygame.K_RIGHT]:
 		screenX += 1
 	if inKeys[pygame.K_LEFT]:
@@ -70,12 +103,16 @@ while True:
 
 	chunkGenerated = False
 	screen.fill((0, 0, 0))
+	chunkLock.acquire()
+	chunkCond.acquire()
 	for x in range(screenX // CHUNK_SIZE, (width + screenX) // CHUNK_SIZE + 1):
 		for y in range(screenY // CHUNK_SIZE, (height + screenY) // CHUNK_SIZE + 1):
 			if (x, y) in surfaces:
 				screen.blit(surfaces[x,y], (CHUNK_SIZE * x - screenX, CHUNK_SIZE * y - screenY))
-			elif not chunkGenerated:
-				chunkGenerated = True
-				surfaces[x, y] = genChunk(x, y)
-
+			else:
+				surfaces[x, y] = pygame.Surface((0, 0))
+				chunkQueue.append((x, y))
+				chunkCond.notify()
+	chunkCond.release()
+	chunkLock.release()
 	pygame.display.flip()
