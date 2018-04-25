a29afff8a6c00d5698eb53a6f7f67c9699e25fdf
procgen <joshlorenzen49@gmail.com>
2018-04-25 09:51:15 -0700

    added landscape subclass

--- Landscape.py
+++ Landscape.py
@@ -125,7 +125,31 @@ class PerlinGenerator(Generator):
 		color = self.getColor(self.perlin.octave(i * self.PIXEL_SIZE + x * self.CHUNK_SIZE, j * self.PIXEL_SIZE + y * self.CHUNK_SIZE, 3, 0.5))
 		return self.colorToInt(color)
 
-gen = PerlinGenerator()
+class LandscapeGenerator(PerlinGenerator):
+
+	def getColor(self, height):
+		if height <= 0.20:
+			return (105,210,231)
+		if height <= 0.34:
+			return (167,219,216)
+		if height <= 0.37:
+			return (224,228,204)
+		if height <= 0.4:
+			return (186,219,129)
+		if height <= 0.6:
+			return (124,197,130)
+		if height <= 0.68:
+			return (62,175,131)
+		if height <= 0.75:
+			return (143,154,156)
+		if height <= 0.8:
+			return (190,195,188)
+		if height <= 0.85:
+			return (215,218,207)
+		else:
+			return (230,232,227)
+
+gen = LandscapeGenerator()
 
 while True:
 	if gen.update() == False:
