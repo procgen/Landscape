14f24aba477c51060530ca64a9fefdd6c09f7f8a
procgen <joshlorenzen49@gmail.com>
2018-04-25 09:39:35 -0700

    Working object encapsulation

--- Landscape.py
+++ Landscape.py
@@ -2,140 +2,140 @@ import sys, pygame, Perlin, math, threading
 import pygame.surfarray as surfarray
 import time
 import numpy
-pygame.init()
 
-size = width, height = 800, 800
+class Generator():
+	#CONSTANTS
+	CHUNK_SIZE = 128
+	PIXEL_SIZE = 4
+	EXITING = False
+	MOVE_SPEED = 0.4
+	size = width, height = 800, 800
 
-screen = pygame.display.set_mode(size)
+	def __init__(self):
 
+		pygame.init()
+		self.screen = pygame.display.set_mode(self.size)
+		self.screen.fill((0, 0, 0))
+		self.screenX = 0
+		self.screenY = 0
 
-#CONSTANTS
-CHUNK_SIZE = 128
-PIXEL_SIZE = 4
-EXITING = False
-MOVE_SPEED = 0.4
+		#THREADING
+		self.surfaces = {}
+		self.chunkQueue = []
+		self.chunkCond = threading.Condition()
+		self.chunkLock = threading.Lock()
 
-screen.fill((0, 0, 0))
-screenX = 0
-screenY = 0
+		self.perlin = Perlin.Perlin(350)
+		#y = 0
 
-surfaces = {}
-chunkQueue = []
-chunkCond = threading.Condition()
-chunkLock = threading.Lock()
+		self.chunkThread = threading.Thread(target=self.chunkThread)
+		self.chunkThread.start()
 
-def getColor(height):
-	if height <= 0.20:
-		return (105,210,231)
-	if height <= 0.34:
-		return (167,219,216)
-	if height <= 0.37:
-		return (224,228,204)
-	if height <= 0.4:
-		return (186,219,129)
-	if height <= 0.6:
-		return (124,197,130)
-	if height <= 0.68:
-		return (62,175,131)
-	if height <= 0.75:
-		return (143,154,156)
-	if height <= 0.8:
-		return (190,195,188)
-	if height <= 0.85:
-		return (215,218,207)
-	else:
-		return (230,232,227)
+		self.clock = pygame.time.Clock()
 
-def getPixel(x, y, i, j):
-	color = getColor(perlin.octave(i * PIXEL_SIZE + x * CHUNK_SIZE, j * PIXEL_SIZE + y * CHUNK_SIZE, 3, 0.5))
-	colorInt = (color[0] << 16) + (color[1] << 8) + (color[2])
-	return colorInt
+	def getColor(self, height):
+		if height <= 0.20:
+			return (105,210,231)
+		if height <= 0.34:
+			return (167,219,216)
+		if height <= 0.37:
+			return (224,228,204)
+		if height <= 0.4:
+			return (186,219,129)
+		if height <= 0.6:
+			return (124,197,130)
+		if height <= 0.68:
+			return (62,175,131)
+		if height <= 0.75:
+			return (143,154,156)
+		if height <= 0.8:
+			return (190,195,188)
+		if height <= 0.85:
+			return (215,218,207)
+		else:
+			return (230,232,227)
 
-def genChunk(x, y):
-	#surface = pygame.Surface((CHUNK_SIZE, CHUNK_SIZE))
-	pixels = numpy.zeros((CHUNK_SIZE, CHUNK_SIZE), numpy.int32)
-	for i in range(CHUNK_SIZE // PIXEL_SIZE):
-		for j in range(CHUNK_SIZE // PIXEL_SIZE):
-			colorInt = getPixel(x, y, i, j)
-			for xOffset in range(PIXEL_SIZE):
-				for yOffset in range(PIXEL_SIZE):
-					pixels[i * PIXEL_SIZE + xOffset, j * PIXEL_SIZE + yOffset] = int(colorInt)
-					#surface.set_at((i * PIXEL_SIZE + xOffset, j * PIXEL_SIZE + yOffset), color)
-	surface = pygame.Surface((128, 128))
-	surfarray.blit_array(surface, pixels)
-	return surface
+	def getPixel(self, x, y, i, j):
+		color = self.getColor(self.perlin.octave(i * self.PIXEL_SIZE + x * self.CHUNK_SIZE, j * self.PIXEL_SIZE + y * self.CHUNK_SIZE, 3, 0.5))
+		colorInt = (color[0] << 16) + (color[1] << 8) + (color[2])
+		return colorInt
 
-#endless loop that handles generating chunks in the chunk queue
-def chunkThread():
-	while not EXITING: #don't get stuck if exiting here either
-		chunkCond.acquire()
-		while chunkQueue == [] and not EXITING: #don't get stuck if exiting
-			chunkCond.wait()
-		if EXITING: break #helps prevent errors when exiting
-		chunkCoords = chunkQueue.pop(0)
-		chunkCond.release()
-		surface = genChunk(chunkCoords[0], chunkCoords[1])
-		chunkLock.acquire()
-		surfaces[chunkCoords[0], chunkCoords[1]] = surface
-		chunkLock.release()
+	def genChunk(self, x, y):
+		#surface = pygame.Surface((CHUNK_SIZE, CHUNK_SIZE))
+		pixels = numpy.zeros((self.CHUNK_SIZE, self.CHUNK_SIZE), numpy.int32)
+		for i in range(self.CHUNK_SIZE // self.PIXEL_SIZE):
+			for j in range(self.CHUNK_SIZE // self.PIXEL_SIZE):
+				colorInt = self.getPixel(x, y, i, j)
+				for xOffset in range(self.PIXEL_SIZE):
+					for yOffset in range(self.PIXEL_SIZE):
+						pixels[i * self.PIXEL_SIZE + xOffset, j * self.PIXEL_SIZE + yOffset] = int(colorInt)
+						#surface.set_at((i * PIXEL_SIZE + xOffset, j * PIXEL_SIZE + yOffset), color)
+		surface = pygame.Surface((128, 128))
+		surfarray.blit_array(surface, pixels)
+		return surface
 
-#function to help cleanup chunkThread for exiting
-def cleanup():
-	global EXITING
-	EXITING = True #tell the thread to exit
-	chunkCond.acquire() #acquire this just to notify..
-	chunkCond.notify() #make sure the thread wakes up so it can end
-	chunkCond.release() #just in case
-	chunkThread.join() #make sure it ends before the main thread
+	#endless loop that handles generating chunks in the chunk queue
+	def chunkThread(self):
+		while not self.EXITING: #don't get stuck if exiting here either
+			self.chunkCond.acquire()
+			while self.chunkQueue == [] and not self.EXITING: #don't get stuck if exiting
+				self.chunkCond.wait()
+			if self.EXITING: break #helps prevent errors when exiting
+			chunkCoords = self.chunkQueue.pop(0)
+			self.chunkCond.release()
+			surface = self.genChunk(chunkCoords[0], chunkCoords[1])
+			self.chunkLock.acquire()
+			self.surfaces[chunkCoords[0], chunkCoords[1]] = surface
+			self.chunkLock.release()
 
-def update():
-	global screenX
-	global screenY
-	dt = clock.tick(60)
+	#function to help cleanup chunkThread for exiting
+	def cleanup(self):
+		self.EXITING = True #tell the thread to exit
+		self.chunkCond.acquire() #acquire this just to notify..
+		self.chunkCond.notify() #make sure the thread wakes up so it can end
+		self.chunkCond.release() #just in case
+		self.chunkThread.join() #make sure it ends before the main thread
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: 
-			cleanup()
-			sys.exit()
+	def update(self):
+		dt = self.clock.tick(60)
 
-	inKeys = pygame.key.get_pressed()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: 
+				self.cleanup()
+				sys.exit()
 
-	if inKeys[pygame.K_ESCAPE]:
-		cleanup()
-		return False
-	if inKeys[pygame.K_RIGHT]:
-		screenX += math.floor(MOVE_SPEED * dt)
-	if inKeys[pygame.K_LEFT]:
-		screenX -= math.floor(MOVE_SPEED * dt)
-	if inKeys[pygame.K_UP]:
-		screenY -= math.floor(MOVE_SPEED * dt)
-	if inKeys[pygame.K_DOWN]:
-		screenY += math.floor(MOVE_SPEED * dt)
+		inKeys = pygame.key.get_pressed()
 
-	chunkGenerated = False
-	screen.fill((0, 0, 0))
-	chunkLock.acquire()
-	chunkCond.acquire()
-	for x in range(screenX // CHUNK_SIZE, (width + screenX) // CHUNK_SIZE + 1):
-		for y in range(screenY // CHUNK_SIZE, (height + screenY) // CHUNK_SIZE + 1):
-			if (x, y) in surfaces:
-				screen.blit(surfaces[x,y], (CHUNK_SIZE * x - screenX, CHUNK_SIZE * y - screenY))
-			else:
-				surfaces[x, y] = pygame.Surface((0, 0))
-				chunkQueue.append((x, y))
-				chunkCond.notify()
-	chunkCond.release()
-	chunkLock.release()
-	pygame.display.flip()
+		if inKeys[pygame.K_ESCAPE]:
+			self.cleanup()
+			return False
+		if inKeys[pygame.K_RIGHT]:
+			self.screenX += math.floor(self.MOVE_SPEED * dt)
+		if inKeys[pygame.K_LEFT]:
+			self.screenX -= math.floor(self.MOVE_SPEED * dt)
+		if inKeys[pygame.K_UP]:
+			self.screenY -= math.floor(self.MOVE_SPEED * dt)
+		if inKeys[pygame.K_DOWN]:
+			self.screenY += math.floor(self.MOVE_SPEED * dt)
 
-perlin = Perlin.Perlin(350)
-y = 0
+		chunkGenerated = False
+		self.screen.fill((0, 0, 0))
+		self.chunkLock.acquire()
+		self.chunkCond.acquire()
+		for x in range(self.screenX // self.CHUNK_SIZE, (self.width + self.screenX) // self.CHUNK_SIZE + 1):
+			for y in range(self.screenY // self.CHUNK_SIZE, (self.height + self.screenY) // self.CHUNK_SIZE + 1):
+				if (x, y) in self.surfaces:
+					self.screen.blit(self.surfaces[x,y], (self.CHUNK_SIZE * x - self.screenX, self.CHUNK_SIZE * y - self.screenY))
+				else:
+					self.surfaces[x, y] = pygame.Surface((0, 0))
+					self.chunkQueue.append((x, y))
+					self.chunkCond.notify()
+		self.chunkCond.release()
+		self.chunkLock.release()
+		pygame.display.flip()
 
-chunkThread = threading.Thread(target=chunkThread)
-chunkThread.start()
-
-clock = pygame.time.Clock()
+gen = Generator()
 
 while True:
-	if update() == False:
+	if gen.update() == False:
 		break
